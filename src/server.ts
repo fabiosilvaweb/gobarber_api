f14723ab297f70f180801942178eb52d25383c0a
Fabio da Silva <fabiodasilva95@outlook.com>
2020-12-15 23:30:55 -0300

    Estrututra Inicial da aplicação

---
+++ src/server.ts
@@ -0,0 +1,12 @@
+import 'dotenv/config';
+import 'reflect-metadata';
+import './typeorm';
+import express from 'express';
+
+const app = express();
+
+app.use(express.json());
+
+app.listen(process.env.PORT, () => {
+  console.log('Servidor rodando!');
+});
